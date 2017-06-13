14522525ef2efec84b0c8782171c06f4c282af9e
yuyangloh <1501568H@student.tp.edu.sg>
2017-06-13 17:32:53 +0800

    (YY) CRUD

--- showhardware_api.py
+++ showhardware_api.py
@@ -1,8 +1,28 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response, url_for
 import subprocess
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
 child = subprocess.Popen('cat showhardware.json',stdout=subprocess.PIPE,shell=True)
 output = child.communicate()[0]
@@ -10,6 +30,7 @@ output = child.communicate()[0]
 tasks = output
 
 @app.route('/vpp/tasks', methods=['GET'])
+@app.login_required
 def READ2():
 
         #Updating JSON File & Flask Item Table
