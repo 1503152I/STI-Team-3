8705ab9dc4954d867fa5abcb071a224f55b533c3
yuyangloh <1501568H@student.tp.edu.sg>
2017-06-13 17:49:57 +0800

    (YY) CRUD

--- bridgedomainlearnapi.py
+++ bridgedomainlearnapi.py
@@ -1,8 +1,28 @@
 #!flask/bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response, url_for
 import subprocess
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler(403)
+def unauthorized(error):
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
 child = subprocess.Popen('cat showbridge.json',stdout=subprocess.PIPE,shell=True)
 output = child.communicate()[0]
@@ -10,6 +30,7 @@ output = child.communicate()[0]
 tasks = output
 
 @app.route('/vpp/tasks', methods=['GET'])
+@app.login_required
 def READ():
 
         #Updating JSON File & Flask Item Table
@@ -21,7 +42,8 @@ def READ():
 
         return tasks
 
-@app.route('/vpp/tasks', methods=['UPDATE'])
+@app.route('/vpp/tasks', methods=['PUT'])
+@app.login_required
 def UPDATElearn():
 
         if not request.json or not 'Learning' in request.json:
