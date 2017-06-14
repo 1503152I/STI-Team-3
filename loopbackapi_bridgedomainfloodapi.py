f084b7fb3ee74a1f7e04ece720075212ef61e16b
yuyangloh <1501568H@student.tp.edu.sg>
2017-06-14 12:41:10 +0800

    (YU YANG) CRUD
    loopbackapi_bridgedomainfloodapi.py - contains full set of CRUD (show bridge-domain, create & delete loopback interface, & set bridge-domain flood)
    showhardware_api.py - contains READ (show hardware-interfaces)
    bridgedomainlearnapi.py - contain UPDATE (set bridge-domain learn)
    vxlan.py - contain CREATE & DELETE (create vxlan tunnel & create vxlan tunnel [...] del)

---
+++ loopbackapi_bridgedomainfloodapi.py
@@ -0,0 +1,93 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, abort, make_response, url_for
+import subprocess
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+child = subprocess.Popen('cat showbridge.json',stdout=subprocess.PIPE,shell=True)
+output = child.communicate()[0]
+
+tasks = output
+
+@app.route('/vpp/tasks', methods=['GET'])
+@auth.login_required
+def READ():
+
+	#Updating JSON File & Flask Item Table
+	updatejson = "python showbridgedomain.py"
+        subprocess.call(updatejson, shell=True)
+        updatedchild = subprocess.Popen('cat showbridge.json', stdout=subprocess.PIPE,shell=True)
+        updatedoutput = updatedchild.communicate()[0]
+        tasks = updatedoutput
+
+        return tasks
+
+@app.route('/vpp/tasks', methods=['POST'])
+@auth.login_required
+def CREATE():
+	 
+        addintf = 'sudo vppctl create loopback interface'
+        subprocess.call(addintf,shell=True)
+
+	return ''
+	
+@app.route('/vpp/tasks', methods=['DELETE'])
+@auth.login_required
+def DELETE():
+
+	if not request.json or not 'Name' in request.json:
+		abort(400)
+
+	task = { 'Name': request.json['Name'] }
+
+	LBintf = str(request.json['Name'])
+	deleteintf = 'sudo vppctl delete loopback interface intfc %s' % (LBintf)
+	subprocess.call(deleteintf, shell=True)
+
+	return ''
+
+@app.route('/vpp/tasks', methods=['PUT'])
+@auth.login_required
+def UPDATEflood():
+
+	if not request.json or not 'Flooding' in request.json:
+		abort(400)
+
+	task = {'Flooding': request.json['Flooding'] }
+
+	bridge = str(request.json['Flooding'])
+	update = 'sudo vppctl set bridge-domain flood %s' % (bridge)
+	subprocess.call(update,stdout=subprocess.PIPE, shell=True)
+	
+	#Updating JSON File & Flask Item Table	
+	updatejson = "python showbridgedomain.py"
+	subprocess.call(updatejson, shell=True)
+	updatedchild = subprocess.Popen('cat showbridge.json', stdout=subprocess.PIPE,shell=True)
+	updatedoutput = updatedchild.communicate()[0]
+	tasks = updatedoutput
+
+	return tasks
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ vxlan.py
@@ -0,0 +1,75 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, abort, make_response, url_for
+import subprocess
+from flask.ext.httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+
+
+@app.route('/vpp/tasks', methods=['POST'])
+@auth.login_required
+def CREATE():
+	
+	if not request.json or not 'local' in request.json:
+		abort(400)
+	if not  request.json or not 'remote' in request.json:
+		abort(400)
+	if not request.json or not 'nn' in request.json:
+		abort(400)
+	
+	task = { 'local': request.json['local'], 'remote': request.json['remote'], 'nn': request.json['nn'] }
+
+	local = str(request.json['local'])
+	remote = str(request.json['remote'])
+	nn = str(request.json['nn'])
+
+        vxlan = 'sudo vppctl create vxlan tunnel src %s dst %s vni %s' % (local, remote, nn)
+        subprocess.call(vxlan,shell=True)
+
+	return ''
+	
+@app.route('/vpp/tasks', methods=['DELETE'])
+@auth.login_required
+def DELETE():
+
+        if not request.json or not 'local' in request.json:
+                abort(400)
+        if not  request.json or not 'remote' in request.json:
+                abort(400)
+        if not request.json or not 'nn' in request.json:
+                abort(400)
+
+        task = { 'local': request.json['local'], 'remote': request.json['remote'], 'nn': request.json['nn'] }
+
+        local = str(request.json['local'])
+        remote = str(request.json['remote'])
+        nn = str(request.json['nn'])
+
+	deletevxlan = 'sudo vppctl create vxlan tunnel src %s dst %s vni %s del' % (local, remote, nn)
+	subprocess.call(deletevxlan, shell=True)
+
+	return ''
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
